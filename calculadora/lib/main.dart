78252b46100878c9931d46a2b1931d8f019329f7
André Marks <andrem4rks@gmail.com>
2023-07-01 23:48:37 -0500

    aula 1 - criacao de app calculadora

---
+++ calculadora/lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text("Calculadora"),
+          ),
+        ),
+        body: const Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Number',
+                  style: TextStyle(fontSize: 70),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  'AC',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(''),
+                Text(''),
+                Text(
+                  '<X',
+                  style: TextStyle(fontSize: 48),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  '7',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '8',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '9',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '/ ',
+                  style: TextStyle(fontSize: 48),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  '4',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '5',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '6',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  'x',
+                  style: TextStyle(fontSize: 48),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  '1',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '2',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '3',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '-',
+                  style: TextStyle(fontSize: 48),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  '0',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  ',',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '=',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '+',
+                  style: TextStyle(fontSize: 48),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
